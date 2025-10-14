c5c86a89270a54b22f6813c456057511b1e1dd6f
Rudranil Bhattacharya <100760323+rudraa2005@users.noreply.github.com>
2025-10-15 02:41:01 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/yourusername/crypto-price-tracker/internal/crypto"
+	"github.com/rudraa2005/crypto-price-tracker/internal/crypto"
 )
 
 func priceHandler(w http.ResponseWriter, r *http.Request) {
